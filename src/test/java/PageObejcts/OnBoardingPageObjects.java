8ed98b5e4be8d3c83d9d8610b5bdc0501240b2c1
DESKTOP-PKMHVU7\agile <Krunal.j@agileinfoways.com>
2021-09-23 15:37:17 +0530

    code update

--- src/test/java/PageObejcts/OnBoardingPageObjects.java
+++ src/test/java/PageObejcts/OnBoardingPageObjects.java
@@ -1,4 +1,25 @@
 package PageObejcts;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class OnBoardingPageObjects {
+
+    WebDriver webDriver;
+
+    public OnBoardingPageObjects(WebDriver webDriver)
+    {
+        this.webDriver = webDriver;
+        PageFactory.initElements(webDriver,this);
+    }
+
+    ///////////////////////////////THIS OBJECTS ONLY FOR THE ONBOARDING PAGE OF PACIFIC54 PROJECT///////////////////////
+
+    @FindBy(xpath = "//div[contains(text(),'Please choose at least a location before continuing.')]")
+    public WebElement PopUpErrorMSG;
+
+    @FindBy(xpath = "//button[@class='Toastify__close-button Toastify__close-button--light']")
+    public WebElement PopUpErrorMSGCancel;
 }
